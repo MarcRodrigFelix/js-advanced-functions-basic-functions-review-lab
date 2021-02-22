2c453b27e97380405fd3d1d92e0718c209ebffd9
Marcos <m.rodrig.felix@gmail.com>
2021-02-21 21:54:11 -0800

    commit tree 08780f8b1f70f8237810e58653aacf578e028482

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+function saturdayFun(activity = 'roller-skate'){
+  return  `This Saturday, I want to ${activity}!`
+};
+
+
+const mondayWork = function(task = 'go to the office'){
+  return `This Monday, I will ${task}.`
+};
+
+
+function wrapAdjective(visualFlair = '*'){
+  return function(param = 'special'){
+    return `You are ${visualFlair}${param}${visualFlair}!` 
+  }
+};
+
+
+const Calculator = {
+  add: function(one, three){ return one + three },
+  subtract: function(one, three){ return one - three },
+  multiply: function(one, three){ return one * three },
+  divide: function(ten, five){ return ten / five }
+};
+
+
+
+function actionApplyer(startingInt, funksArray){
+  let sumOfInt = startingInt;
+
+  if (funksArray.length === 0){
+    return startingInt
+  } else {
+    funksArray.forEach(function(funk){
+      sumOfInt = funk(sumOfInt)
+    })
+    return sumOfInt;
+  }
+}
